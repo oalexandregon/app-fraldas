5a38890add7dd4a91e413e909a7eafb01509e8bb
Alexandre Goncalves <avg1202@gmail.com>
2024-11-04 13:55:32 -0300

    Configuração das bibliotecas SASS, React Router DOM, MUI e organização de diretórios no projeto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>HOME</p>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/button/index.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components";
+
+function Button() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+  export default App;
